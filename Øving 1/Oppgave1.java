1ec8f3a7e4794f62ae898c6b3d3c4f4f37f18fde
HenrikJohnsrud <72154180+HenrikJohnsrud@users.noreply.github.com>
2020-09-30 18:28:19 +0200

    Create Oppgave1.java

---
+++ Øving 1/Oppgave1.java
@@ -0,0 +1,9 @@
+import static javax.swing.JOptionPane.*;
+class Oving1_1 {
+    public static void main(String[] args) {
+        String lengdeLest = showInputDialog("Lengde (Tommer): ");
+        double lengde= Double.parseDouble(lengdeLest);
+        double resultat = lengde * 2.54;
+        showMessageDialog(null, lengde + " tommer = " + resultat + " centimeter");
+    }
+}
